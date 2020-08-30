e8e3966c772b9d6ff4c7dd7f9728490c8242e271
thalvari <tuomas.halvari@helsinki.fi>
2020-08-30 23:23:45 +0300

    small fix

--- pyzoo/zoo/automl/search/RayTuneSearchEngine.py
+++ pyzoo/zoo/automl/search/RayTuneSearchEngine.py
@@ -202,7 +202,7 @@ class RayTuneSearchEngine(SearchEngine):
         #         verbose=1,
         #         reuse_actors=True
         #         )
-        self.trials = [trial for trial in trials if trial.is_finished()]
+        self.trials = [trial for trial in trials if trial.status == "TERMINATED"]
         return self
 
     def get_best_trials(self, k=1):
